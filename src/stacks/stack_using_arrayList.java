590f6e260602c4b99fce30ad8f06498a125b444d
Aakash Tyagi <78840211+AakashTyagi354@users.noreply.github.com>
2022-09-18 09:56:06 +0530

    stack implementation using arrayList

---
+++ src/stacks/stack_using_arrayList.java
@@ -0,0 +1,45 @@
+package stacks;
+
+import java.util.ArrayList;
+
+public class stack_using_arrayList {
+    static class stack{
+        static ArrayList<Integer> list = new ArrayList<>();
+
+        public static boolean isEmpty(){
+            return list.size() == 0;
+        }
+
+        // push
+        public static void push(int data){
+            list.add(data);
+        }
+
+        // pop
+        public static int pop(){
+            if(list.isEmpty()){
+                return -1;
+            }
+            int top = list.get(list.size()-1);
+            list.remove(list.size()-1);
+            return top;
+        }
+
+        // peek
+        public static int peek(){
+            return list.get(list.size()-1);
+        }
+    }
+    public static void main(String[] args) {
+        stack s = new stack();
+        s.push(10);
+        s.push(20);
+        s.push(30);
+
+        while (!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+
+    }
+}
